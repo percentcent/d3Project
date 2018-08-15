ae98fda6d3b75cbb9aebbfc8cfce0ccc44bb4428
peishanLee <lpszoe1214@gmail.com>
2018-08-15 18:45:51 +1000

    preprocess update

--- preprocess.py
+++ preprocess.py
@@ -1,28 +1,30 @@
 
-from nltk.corpus import stopwords
-stopWords = set(stopwords.words('english'))
-print(stopWords)
-
 count = 0
-fo = open("/Users/peishanlee/Downloads/tweets_au_vic_full.txt","r")
-fw = open('tweetProcessed_subset.txt', 'w')
-for line in open('/Users/peishanlee/Downloads/tweets_au_vic_full.txt'):
-	if(count > 200000):
-		break
-	count += 1
-	line = fo.readline()
+fw = open('/Users/peishanlee/Downloads/tweet_GreatMelbourne.txt', 'w')
+
+maxlon = 144.0
+for line in open("/Users/peishanlee/Downloads/tweets_au_vic_full.txt","r"):
 	elem = line.split('	')
 	if (len(elem) < 6):
 		continue
 	tid = elem[0]
 	time = elem[1]
 	text = elem[2]
-	hashtags = elem[3]
-	keyword = elem[4]
+	#hashtags = elem[3]
+	#keyword = elem[4]
 	lat = elem[5]
 	lon = elem[6]
-	fw.write(elem[0]+"\t"+elem[1]+"\t"+elem[2]+"\t"+elem[3]+"\t"+elem[4]+"\t"+elem[5]+"\t"+elem[6]+"\n")
+	if(count >0):
+		try:
+			latNum = float(lat)
+			lonNum = float(lon)
+			if lonNum > maxlon:
+				maxlon = lonNum
+			if latNum > -38.10 and latNum < -37.50 and lonNum > 144.70 and lonNum < 145.45:
+				fw.write(elem[0]+"\t"+elem[1]+"\t"+elem[2]+"\t"+elem[5]+"\t"+elem[6]+"\n")
+		except ValueError:
+			print("error")
+	count += 1
 
-fo.close()
 fw.close()
-
+print(maxlon)
